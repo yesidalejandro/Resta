22385eabfbb4dd2fdcbd2c5366fcfbd2e167489d
principal <principal@admin>
2016-02-03 06:38:14 -0500

    primera modificacion de archivos de la  resta

---
+++ Resta/src/resta/Resta.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resta;
+
+/**
+ *
+ * @author principal
+ */
+public class Resta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        System.out.println("a - b = " + (a - b) );
+    }
+    
+}
+    
+    
+
